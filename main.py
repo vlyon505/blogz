e0ad83074d4648cab7b71a433835f2248fa936ac
Victoria Lyon <vlyon505@gmail.com>
2017-04-28 15:08:39 -0500

    Blogz assignment completed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import webapp2, jinja2, os, re
+import webapp2, jinja2, os, re, cgi
 from google.appengine.ext import db
 from models import Post, User
 import hashutils
@@ -21,7 +21,8 @@ class BlogHandler(webapp2.RequestHandler):
         """
 
         # TODO - filter the query so that only posts by the given user
-        return None
+        query = Post.all().filter("author",user).order('-created')
+        return query.fetch(limit=limit, offset=offset)
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
